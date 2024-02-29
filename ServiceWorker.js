13c36a6fefa77cda9c27ce117fdcdb3a3c6de35b
Egan Wall <eganwall@users.noreply.github.com>
2024-02-29 00:05:54 -0500

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-kardashev_proto-1.0";
 const contentToCache = [
-    "Build/20240221_web.loader.js",
-    "Build/20240221_web.framework.js.unityweb",
-    "Build/20240221_web.data.unityweb",
-    "Build/20240221_web.wasm.unityweb",
+    "Build/20240228_web.loader.js",
+    "Build/20240228_web.framework.js.unityweb",
+    "Build/20240228_web.data.unityweb",
+    "Build/20240228_web.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
